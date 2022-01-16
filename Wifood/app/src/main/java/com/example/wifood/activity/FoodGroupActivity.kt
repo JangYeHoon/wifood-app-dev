d318608a8c3f638d64f601ac2e961050df107210
JangYeHoon <jkjk128jkjk@gmail.com>
2022-01-16 20:22:13 +0900

    Add layout & firebases setting

---
+++ Wifood/app/src/main/java/com/example/wifood/activity/FoodGroupActivity.kt
@@ -0,0 +1,14 @@
+package com.example.wifood.activity
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.wifood.R
+
+class FoodGroupActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_group)
+    }
+}
---
+++ Wifood/app/src/main/java/com/example/wifood/entity/Group.kt
@@ -0,0 +1,4 @@
+package com.example.wifood.entity
+
+class Group {
+}
