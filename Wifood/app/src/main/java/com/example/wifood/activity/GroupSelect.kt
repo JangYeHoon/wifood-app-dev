a4dc42f0e68783ada4edca88648315e9c094b835
JangYeHoon <jkjk128jkjk@gmail.com>
2022-01-31 22:40:26 +0900

    Add GroupSelect Activity

---
+++ Wifood/app/src/main/java/com/example/wifood/activity/GroupSelect.kt
@@ -0,0 +1,20 @@
+package com.example.wifood.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wifood.databinding.ActivityGroupSelectBinding
+
+class GroupSelect : AppCompatActivity() {
+    lateinit var binding : ActivityGroupSelectBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGroupSelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.foodGroupBtn.setOnClickListener {
+            val intent = Intent(this@GroupSelect, FoodGroup::class.java)
+            startActivity(intent)
+        }
+    }
+}
