a49b1c700b4809ea86c558e9b0ec1166f352dcac
kimdukbae <han885404@naver.com>
2022-02-02 01:35:53 +0900

    merge project

---
+++ Wifood/app/src/main/java/com/example/wifood/activity/Map.kt
@@ -0,0 +1,73 @@
+package com.example.wifood.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.annotation.UiThread
+import com.example.wifood.R
+
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
+
+class Map : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        val btn = findViewById<Button>(R.id.goGroupSelect) as Button
+        btn.setOnClickListener {
+            val intent = Intent(this, GroupSelect::class.java)
+            startActivity(intent)
+        }
+
+        // 지도 객체 생성
+        val fm = supportFragmentManager // 다른 Fragment내에 배치할 경우 childFragmentManager로
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+
+        // getMapAsync()를 호출하여 비동기로 onMapReady 콜백 메서드 호출
+        // onMapReady에서 NaverMap 객체를 받음
+        mapFragment.getMapAsync(this)
+
+        // 위치를 반환하는 구현체인 FusedLocationSource 생성
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            // 권한 거부됨
+            if(!locationSource.isActivated) {
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+        // naverMap 객체 받아서 naverMap 객체에 위치 소스 지정
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+
+        // 현재위치 표시
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
+        naverMap.locationTrackingMode = LocationTrackingMode.Face   // 위치추적 활성화, 현위치 오버레이, 카메라 좌표, 베어링이 사용자의 위치 및 방향에 따라 움직임
+
+        // 지도상에 마커 표시 → 맛집 표시로 활용하면 될 듯
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
+}
