663eddf8dae8f68dcae2b116725bd1a9890fd753
kangjik <kangjik@naver.com>
2022-02-13 19:47:28 +0900

    Find Password UI Complete

---
+++ Wifood/app/src/main/java/com/example/wifood/activity/FindPassword.kt
@@ -0,0 +1,11 @@
+package com.example.wifood.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_password)
+    }
+}
