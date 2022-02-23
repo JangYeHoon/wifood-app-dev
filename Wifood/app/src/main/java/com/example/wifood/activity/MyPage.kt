046823a0b8379a1f6ee6028ec18388d001358cd5
kangjik <kangjik@naver.com>
2022-02-23 22:18:28 +0900

    1. delete user ask again dialog added 2. set user email on mypage 3. set user nickname on mypage 4. logout user added auto login set false when logout or deleted

--- Wifood/app/src/main/java/com/example/wifood/activity/MyPage.kt
+++ Wifood/app/src/main/java/com/example/wifood/activity/MyPage.kt
@@ -1,14 +1,94 @@
 package com.example.wifood.activity
 
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_my_page.*;
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.wifood.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_map.*
 
 class MyPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
 
+        // Firebase
+        val db = Firebase.database;
+        val dbRootPath = "kg_test_db";
+        val dbRef = db.getReference(dbRootPath);
 
+        val userEmail = intent.getStringExtra("UserEmail").toString()
+        // set user nickname
+        dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapShot: DataSnapshot) {
+                val nickname = snapShot.child(userEmail).child("info").child("nickname").value.toString()
+                textViewMyPageUserName.text = nickname
+                textViewMyPageUserEmail.text = userEmail.replace("_com",".com")
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+        // button event : modify my info
+        btnMyPageModifyMyInfo.setOnClickListener {
+            //val intent = Intent(this, Joinin::class.java)
+            //intent.putExtra("UserEmail",userEmail)sssss
+            // joinin 가서 user email가지고 나머지 정보 채우고 deactivate 해줘야함
+                    // 체크도 간소화해서 하자.
+            //startActivity(intent)
+        }
+
+        // button event : modify my favor
+        btnMyPageModifyMyFavor.setOnClickListener {
+            //val intent = Intent(this, ModifyMyFavor::class.java)
+            //intent.putExtra("UserEmail",userEmail)
+            //startActivity(intent)
+        }
+
+        //button event : go option
+
+        // image button event : go option
+
+        // button event : logout
+        btnMyPageLogout.setOnClickListener{
+            val autoLoginPref = getSharedPreferences("wifoodAutoLogin", MODE_PRIVATE);
+            val autoLoginEditor = autoLoginPref.edit();
+            autoLoginEditor.clear();
+            autoLoginEditor.apply();
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        // button event : user delete
+        btnMyPageDeleteUser.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder
+                .setTitle("회원 탈퇴")
+                .setMessage("정말 탈퇴할까요?")
+                .setNegativeButton("취소", DialogInterface.OnClickListener { dialog, which ->
+                })
+                .setPositiveButton("확인", DialogInterface.OnClickListener { dialog,which->
+                    // remove auto login pref
+                    val autoLoginPref = getSharedPreferences("wifoodAutoLogin", MODE_PRIVATE);
+                    val autoLoginEditor = autoLoginPref.edit();
+                    autoLoginEditor.clear();
+                    autoLoginEditor.apply();
+
+                    // remove user info in database
+                    dbRef.child(userEmail).removeValue()
+
+                    val intent = Intent(this, Login::class.java)
+                    startActivity(intent)
+            })
+            builder.show()
+        }
     }
 }
