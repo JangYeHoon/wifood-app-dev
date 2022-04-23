33dbc587f118a64180d2f89d0a2b026d7c7e8d30
KangJik <kangjik94@naver.com>
2022-04-23 21:22:43 +0900

    [FindIdOrPwd]find id and pwd UI updated with findIdOrPwd.kt

--- Wifood/app/src/main/java/com/example/wifood/activity/FindIdOrPwd.kt
+++ Wifood/app/src/main/java/com/example/wifood/activity/FindIdOrPwd.kt
@@ -1,5 +1,6 @@
 package com.example.wifood.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,33 +8,31 @@ import com.example.wifood.databinding.ActivityFindIdOrPwdBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 
 class FindIdOrPwd : AppCompatActivity() {
     private var mBinding: ActivityFindIdOrPwdBinding?=null
     private val binding get() = mBinding!!
+    var imm : InputMethodManager?= null
 
-    override fun onDestroy() {
-        mBinding= null
-        super.onDestroy()
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityFindIdOrPwdBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // add keybord event
+        imm = getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as InputMethodManager?
 
         // Firebase
         val db = Firebase.database;
         val dbRootPath = "kg_test_db";
         val dbRef = db.getReference(dbRootPath);
 
+        // tab layout event
         binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-            override fun onTabReselected(tab: TabLayout.Tab?){
-
-
-            }
-            override fun onTabUnselected(tab: TabLayout.Tab?){
-
-            }
+            override fun onTabReselected(tab: TabLayout.Tab?){}
+            override fun onTabUnselected(tab: TabLayout.Tab?){}
             override fun onTabSelected(tab: TabLayout.Tab?){
                 when(tab!!.position){
                     0-> {
@@ -47,8 +46,45 @@ class FindIdOrPwd : AppCompatActivity() {
                     }
                 }
             }
-
         })
 
+        // back button
+        binding.imageButtonBack.setOnClickListener{
+            val intent = Intent(this@FindIdOrPwd, Login::class.java)
+            startActivity(intent)
+        }
+
+        // find id button
+        binding.findIdTabButtonFindId.setOnClickListener{
+            Toast.makeText(this@FindIdOrPwd, "미구현",Toast.LENGTH_SHORT).show()
+        }
+        // find id auth
+        binding.findIdTabButtonPhoneCheck.setOnClickListener{
+            Toast.makeText(this@FindIdOrPwd, "미구현",Toast.LENGTH_SHORT).show()
+        }
+        // find pwd button
+        binding.findPwdTabButtonFindPwd.setOnClickListener{
+            Toast.makeText(this@FindIdOrPwd, "미구현",Toast.LENGTH_SHORT).show()
+        }
+
+        // find pwd auth
+        binding.findPwdTabButtonPhoneCheck.setOnClickListener{
+            Toast.makeText(this@FindIdOrPwd, "미구현",Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
+    }
+
+    override fun onDestroy() {
+        mBinding= null
+        super.onDestroy()
+    }
+
+    fun hideKeyboard(v : View){
+        if(v != null){
+            imm?.hideSoftInputFromWindow(v.windowToken,0)
+        }
     }
 }
