36be00fcf92d3066aba6666889e97c86781529f5
KangJik <kangjik94@naver.com>
2022-04-24 16:17:18 +0900

    [Modify Password UI] UI and Code new

--- Wifood/app/src/main/java/com/example/wifood/activity/ModifyPassword.kt
+++ Wifood/app/src/main/java/com/example/wifood/activity/ModifyPassword.kt
@@ -1,9 +1,11 @@
 package com.example.wifood.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.example.wifood.databinding.ActivityModifyPasswordBinding
 
 class ModifyPassword : AppCompatActivity() {
@@ -16,6 +18,17 @@ class ModifyPassword : AppCompatActivity() {
         mBinding = ActivityModifyPasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
         imm = getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+
+        binding.buttonBack.setOnClickListener{
+            val intent = Intent(this@ModifyPassword,ModifyMyInfo::class.java)
+            intent.putExtra("UserEmail","testingEmail")
+            startActivity(intent)
+        }
+
+        binding.buttonModifyPassword.setOnClickListener {
+            Toast.makeText(this@ModifyPassword, "미구현", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 
