f05a865c4feb00964b61fbef44dd07cf75abff9a
kmh <dkakqlql@gmail.com>
2022-06-03 18:07:51 +0900

    [PlaceInfo] Add SavedStateHandle

--- Wifood/app/src/main/java/com/example/wifood/presentation/view/placeList/PlaceInfoViewModel.kt
+++ Wifood/app/src/main/java/com/example/wifood/presentation/view/placeList/PlaceInfoViewModel.kt
@@ -3,7 +3,9 @@ package com.example.wifood.presentation.view.placeList
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.wifood.domain.model.Place
 import com.example.wifood.domain.usecase.WifoodUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -13,10 +15,21 @@ import javax.inject.Inject
 // 일단은 PlaceInfoViewModel 이라고 해뒀는데 한 가지 뷰모델이 비슷한 기능으로 여러 곳에서 쓰이면 합쳐도 됨(재활용 가능)
 @HiltViewModel
 class PlaceInfoViewModel @Inject constructor(
-    private val useCases: WifoodUseCases
+    private val useCases: WifoodUseCases,
+    savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     // state
     var state by mutableStateOf(PlaceInfoState())
+
+    init {
+        // 전 화면(그룹리스트)에서 이 화면으로 클릭된 place가 어떤 place인지 알만한 정보를 넘겨줘야 함
+        // 그 때 넘겨준 정보가 savedStateHandle에 담겨있음
+        // 어떤 데이터 형태로 넘겨줄지는 결정해야 함
+        // 아래는 예시
+        savedStateHandle.get<Place>("place")?.let { place ->
+            state = state.copy(placeInfo = place)
+        }
+    }
 
     // event
     fun onEvent(event: PlaceInfoEvent) {
