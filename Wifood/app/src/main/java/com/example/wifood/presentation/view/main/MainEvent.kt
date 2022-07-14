06a7604ec0873fee6b077570452448c398b7b63e
JangYeHoon <jkjk128jkjk@gmail.com>
2022-07-14 22:29:01 +0900

    [Map] Add place search
     - YOGO-155
     - Implementation place search based on current location

--- Wifood/app/src/main/java/com/example/wifood/presentation/view/main/MainEvent.kt
+++ Wifood/app/src/main/java/com/example/wifood/presentation/view/main/MainEvent.kt
@@ -3,6 +3,7 @@ package com.example.wifood.presentation.view.main
 import android.location.Location
 import com.example.wifood.domain.model.Group
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.model.Place
 
 sealed class MainEvent {
     data class ItemClicked(val selected: String) : MainEvent()
@@ -11,4 +12,5 @@ sealed class MainEvent {
     data class DeleteGroupEvent(val groupId: Int) : MainEvent()
     data class LocationChanged(val location: Location) : MainEvent()
     data class CameraMove(val latLng: LatLng) : MainEvent()
+    data class SearchClicked(val searchPlaceResult: Place) : MainEvent()
 }
