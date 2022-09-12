973bf39ebf0feb76b6b75b285280d122f7181b76
kim kangjik <kimkangjik@kims-MacBook-Air.local>
2022-09-12 16:46:52 +0900

    new pages added

---
+++ Wifood/app/src/main/java/com/example/wifood/presentation/view/placeList/newPlaceInfo/PutPlaceAddressList.kt
@@ -0,0 +1,36 @@
+package com.example.wifood.presentation.view.placeList.newPlaceInfo
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.wifood.R
+import com.example.wifood.presentation.view.component.*
+import com.example.wifood.ui.theme.mainFont
+import com.example.wifood.view.ui.theme.*
+
+@Preview(showBackground = true)
+@Composable
+fun PutPlaceAddressList(
+
+){
+    
+}
